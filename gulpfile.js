34c32887e974f0d58e8cc1bb3f3ff32430c437e8
wangpin <pin.wang@hp.com>
2017-04-01 11:03:30 +0800

    Add cleanup dist script

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,30 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename')
 var pump = require('pump');
 var strip = require('gulp-strip-debug');
+var deletefile = require('gulp-delete-file');
+var version = require('./package.json').version;
 
-gulp.task('compress', function (cb) {
-    pump([
-            gulp.src('lib/vue-scroll.js'),
-            uglify(),
-            strip(),
-            rename(function (path) {
-                path.basename = "vue-scroll.min";
-                path.extname = ".js"
-            }),
-            gulp.dest('dist')
-            ],
-            cb);
+gulp.task('clean', function(cb){
+ var regexp = /[^\.].js/ig;
+  pump([
+    gulp.src('dist/*.js'),
+    deletefile({
+      reg: regexp,
+      deleteMatch: true
+    }),
+    ],cb)
+})
+
+gulp.task('compress', ['clean'],function (cb) {
+  pump([
+    gulp.src('lib/vue-scroll.js'),
+    strip(),
+    uglify(),
+    rename(function (path) {
+        path.basename = `vue-scroll-v${version}.min`;
+        path.extname = `.js`
+    }),
+    gulp.dest('dist')
+    ],
+    cb);
 });
