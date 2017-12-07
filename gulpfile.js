659e7cc291dc0a4ff9442db9eacf640c78e4c06c
wangpin34 <270280522@qq.com>
2017-12-07 15:05:16 +0800

    Release v2.1.4.
    Remove console state from publish

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,12 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename')
-var pump = require('pump');
 var strip = require('gulp-strip-debug');
 var deletefile = require('gulp-delete-file');
 var version = require('./package.json').version;
 
-gulp.task('clean', function(cb){
- var regexp = /[^\.].js/ig;
-  pump([
-    gulp.src('dist/*.js'),
-    deletefile({
-      reg: regexp,
-      deleteMatch: true
-    }),
-    ],cb)
+gulp.task('build', function () {
+  gulp.src('src/*.js')
+    .pipe(strip())
+    .pipe(gulp.dest('lib'))
 })
-
-gulp.task('compress', ['clean'],function (cb) {
-  pump([
-    gulp.src('lib/vue-scroll.js'),
-    strip(),
-    uglify(),
-    rename(function (path) {
-        path.basename = `vue-scroll-v${version}.min`;
-        path.extname = `.js`
-    }),
-    gulp.dest('dist')
-    ],
-    cb);
-});
